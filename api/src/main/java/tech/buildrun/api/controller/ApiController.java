96e2d457adb5365538561a18d47241bb8a2ec338
Raimdrs <raiimdrs@gmail.com>
2024-12-08 20:45:40 -0300

    API_REST_Agora_ta_atualizando_tarefa_e_apagando_uma_especifica

--- api/src/main/java/tech/buildrun/api/controller/ApiController.java
+++ api/src/main/java/tech/buildrun/api/controller/ApiController.java
@@ -3,7 +3,9 @@ package tech.buildrun.api.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,7 +26,7 @@ public class ApiController {
     }
 
     @GetMapping(path = "/tasks")
-    public ResponseEntity<String> listTasks() throws JsonProcessingException{
+    public ResponseEntity<String> listTasks() throws JsonProcessingException {
         return ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
     }
 
@@ -37,6 +39,26 @@ public class ApiController {
     @DeleteMapping(path = "/tasks")
     public ResponseEntity<Void> clearTasks() {
         tasks = new ArrayList<>();
-        return ResponseEntity.ok().build();    
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(path = "/tasks/{index}")
+    public ResponseEntity<Void> updateTask(@PathVariable int index, @RequestBody String updatedTask) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.set(index, updatedTask);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping(path = "/tasks/{index}")
+    public ResponseEntity<Void> deleteTask(@PathVariable int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
